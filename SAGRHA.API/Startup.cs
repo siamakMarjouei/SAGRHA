db2b5f9b634b50bda12a52ebfb15c39591c67845
siamak marjouei <siamakmarjouei@gmail.com>
2019-09-20 01:48:54 -0400

    Error handling with Headers and production env

--- SAGRHA.API/Startup.cs
+++ SAGRHA.API/Startup.cs
@@ -1,13 +1,17 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using SAGRHA.API.Data;
+using SAGRHA.API.Helpers;
 
 namespace SAGRHA.API
 {
@@ -50,6 +54,18 @@ namespace SAGRHA.API
             }
             else
             {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //TODO turn off for now 
                 //app.UseHsts();
