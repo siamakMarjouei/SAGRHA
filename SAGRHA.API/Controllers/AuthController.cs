72ee5bb168f7ff6b199108b4c2c8d848cbd251bc
siamak marjouei <siamakmarjouei@gmail.com>
2019-09-21 02:08:19 -0400

    error handling back end and client

--- SAGRHA.API/Controllers/AuthController.cs
+++ SAGRHA.API/Controllers/AuthController.cs
@@ -48,7 +48,7 @@ namespace SAGRHA.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            throw new Exception("Computer says no");
+            
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
