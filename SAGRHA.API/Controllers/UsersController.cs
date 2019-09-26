b9188f8f5a6800342ab1bffe3a131943e09a29f3
siamak marjouei <siamakmarjouei@gmail.com>
2019-09-26 19:07:00 -0400

    updating user

--- SAGRHA.API/Controllers/UsersController.cs
+++ SAGRHA.API/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -41,5 +43,22 @@ namespace SAGRHA.API.Controllers
 
             return Ok(userToReturn);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdatesDto userForUpdatesDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetById(id);
+
+            _mapper.Map(userForUpdatesDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
+        }
+
     }
 }
--- SAGRHA.API/Helpers/AutoMapperProfiles.cs
+++ SAGRHA.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace SAGRHA.API.Helpers
                 .ForMember(dest => dest.Age, 
                 opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdatesDto, User>();
         }
     }
 }
