e42b3d6bf78939006e198570e4bb90f5edee2fad
siamakMarjouei <50041398+siamakMarjouei@users.noreply.github.com>
2019-10-14 16:15:56 -0400

    Child Dto added

---
+++ SAGRHA.API/Dtos/ChildToReturnDto.cs
@@ -0,0 +1,10 @@
+namespace SAGRHA.API.Dtos
+{
+    public class ChildToReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
